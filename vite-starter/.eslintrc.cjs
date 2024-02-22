8a9627ef6064410ad7a97ac0e14d5fbac2ed0536
uuyeong <proyeongs@gmail.com>
2024-02-22 23:10:25 +0900

    eslint testing-library config 추가

--- vite-starter/.eslintrc.cjs
+++ vite-starter/.eslintrc.cjs
@@ -3,22 +3,13 @@ const vitest = require("eslint-plugin-vitest");
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:vitest/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react/jsx-runtime", "plugin:react-hooks/recommended", "plugin:vitest/recommended", "plugin:testing-library/react"],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     "no-unused-vars": "warn", // warning, not error
     "vitest/expect-expect": "off", // eliminate distracting red squiggles while writing tests
     "react/prop-types": "off", // turn off props validation
